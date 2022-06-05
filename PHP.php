aed09590523c480d28db556594fd507bf6d00adb
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-06-05 13:15:10 +0100

    Update PHP.php

--- PHP.php
+++ PHP.php
@@ -7,7 +7,6 @@ $xmlDoc->load("cd_catalog.xml");
 $x=$xmlDoc->getElementsByTagName('ARTIST');
 
 for ($i=0; $i<=$x->length-1; $i++) {
-  //Process only element nodes
   if ($x->item($i)->nodeType==1) {
     if ($x->item($i)->childNodes->item(0)->nodeValue == $q) {
       $y=($x->item($i)->parentNode);
@@ -18,7 +17,6 @@ for ($i=0; $i<=$x->length-1; $i++) {
 $cd=($y->childNodes);
 
 for ($i=0;$i<$cd->length;$i++) {
-  //Process only element nodes
   if ($cd->item($i)->nodeType==1) {
     echo("<b>" . $cd->item($i)->nodeName . ":</b> ");
     echo($cd->item($i)->childNodes->item(0)->nodeValue);
